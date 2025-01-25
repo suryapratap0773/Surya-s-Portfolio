7bebbe5618694f304313d29106cbc85d57f8a3ba
Surya Pratap Verma <154822822+suryapratap0773@users.noreply.github.com>
2025-01-25 06:17:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Student", "Web Developer", "Web Designer", "MERN Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
